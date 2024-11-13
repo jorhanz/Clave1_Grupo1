76a15aeb69c3606817220074c2cd42dd8757524d
Luis Pinto <jorgeluispinto2000@gmail.com>
2024-11-13 00:29:30 -0600

    Experiente clinico creacion de clases

---
+++ CapaDatos/ExpedienteDatos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace CapaDatos
+{
+    public class ExpedienteDatos
+    {
+        public bool InsertarConsulta(int idMascota, int idVeterinario, DateTime fechaConsulta, string descripcion)
+        {
+            using (MySqlConnection conexion = new Conexion().GetConnection())
+            {
+                conexion.Open();
+                string query = @"INSERT INTO ExpedienteClinico (FechaConsulta, Descripcion, IdMascota, IdVeterinario) 
+                         VALUES (@FechaConsulta, @Descripcion, @IdMascota, @IdVeterinario)";
+                MySqlCommand cmd = new MySqlCommand(query, conexion);
+                cmd.Parameters.AddWithValue("@FechaConsulta", fechaConsulta);
+                cmd.Parameters.AddWithValue("@Descripcion", descripcion);
+                cmd.Parameters.AddWithValue("@IdMascota", idMascota);
+                cmd.Parameters.AddWithValue("@IdVeterinario", idVeterinario);
+
+                return cmd.ExecuteNonQuery() > 0;
+            }
+        }
+        public DataTable ObtenerHistorialClinico(int idMascota)
+        {
+            using (MySqlConnection conexion = new Conexion().GetConnection())
+            {
+                conexion.Open();
+                string query = @"SELECT e.IdExpediente, e.FechaConsulta, e.Descripcion, u.NombreCompleto AS NombreVeterinario
+                         FROM ExpedienteClinico e
+                         INNER JOIN Usuarios u ON e.IdVeterinario = u.IdUsuario
+                         WHERE e.IdMascota = @IdMascota";
+                MySqlCommand cmd = new MySqlCommand(query, conexion);
+                cmd.Parameters.AddWithValue("@IdMascota", idMascota);
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                DataTable historial = new DataTable();
+                adapter.Fill(historial);
+                return historial;
+            }
+        }
+    }
+}
---
+++ CapaNegocio/ExpedienteNegocio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using System.Data;
+
+namespace CapaNegocio
+{
+    public class ExpedienteNegocio
+    {
+        private ExpedienteDatos expedienteDatos = new ExpedienteDatos();
+
+        public bool RegistrarConsulta(int idMascota, int idVeterinario, DateTime fechaConsulta, string descripcion)
+        {
+            return expedienteDatos.InsertarConsulta(idMascota, idVeterinario, fechaConsulta, descripcion);
+        }
+
+        public DataTable ObtenerHistorialClinico(int idMascota)
+        {
+            return expedienteDatos.ObtenerHistorialClinico(idMascota);
+        }
+    }
+}
